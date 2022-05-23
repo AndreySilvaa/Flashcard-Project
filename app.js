83e851d104c16c2e85c533051aa122eb7e46ef5f
Andrey Silva Lima <andreysl2001@gmail.com>
2022-05-23 19:39:27 -0300

    Flash-Card adicionado na tela

--- app.js
+++ app.js
@@ -1,131 +1,104 @@
+//Criar o op(objeto principal)
+//Verificar se existe localStorage
+//se existe somar o id do último, se não criar o primeiro id
 //abrir e fechar formulário
 //criar a class, pegar as informações do form e criar um novo objeto
 //passar as informações do novo objeto para o localstorage
 //criar um método para pegar as informações e criar um flashcard
 
-(function(){
-    const addbt = document.getElementById("add")
-    const form = document.getElementById("createcard")
-    const close = document.getElementById("close")
-    const save = document.getElementById("save")
-    const colors = ["#CED971", "#E19853", "#77BAE4", "#CF5E9E", "#A577B8", "#E1A8C4"]
+function inicia(){
+    const btadd = document.getElementById("add")
+    const cardform = document.getElementById("createcard")
+    const btclose = document.getElementById("close")
+    const form = document.getElementById("form")
     const feedback = document.getElementById("feedback")
-    var flashcards = []
+    const dvcards = document.getElementById("cards")
 
+    let id
 
-    addbt.addEventListener("click", () =>{
-        form.classList.remove("hidden")
+    let op = new OP()
+
+    //Coletando as informações do localStorage
+    let data = op.recolherLocalStorage() 
+    if(data.length > 0){
+        id = (data[data.length-1].id) + 1
+    }else{
+        id = 1
+    }
+    console.log(data.length)
+    console.log(data)
+    data.forEach((item) => {
+        op.addCard(item)
     })
 
-    close.addEventListener("click", () =>{
-        form.classList.add("hidden")
+    //Abrindo o form
+    btadd.addEventListener("click", () =>{
+        op.showCardForm()
     })
 
+    //Fechando o form
+    btclose.addEventListener("click", () =>{
+        op.hideCardForm()
+    })
 
-    // ADICIONANDO A CLASS  
-    class flashcard{
-        constructor(question, answer){
-            this.question = question
-            this.answer = answer
-        }
-    }
-
-    class Display{}
-
-    // CHAMANDO O LOCALSTORAGE
-    function getstorage(){
-        let cards = localStorage.getItem("cards")
-        if(cards == null){
-            flashcards = []
-        }else{
-            flashcards = JSON.parse(cards)
-            getlocalstorage(flashcards)
-        }
-    }
-
-    getstorage()
-
-    // COLETANDO AS INFORMAÇÕES DO FORM
-    save.addEventListener("click", (e) =>{
+    //Criando o flash-card
+    form.addEventListener("submit", (e) =>{
         e.preventDefault()
+        const question = document.getElementById("questao").value
+        const answer = document.getElementById("resposta").value
 
-        let que = document.getElementById("questao")
-        let re = document.getElementById("resposta")
-
-        let card = new flashcard(que.value, re.value)
-        card.checkinfo()
-        //let display = new Display()
-        //flashcards.push(card)
-        //display.passinfo(flashcards)
-        //let item = [flashcards[flashcards.length-1]]
-        //getlocalstorage(item)
-        //form.classList.add("hidden")
-        que.innerText = ''
-        re.innerText = ""
+        if(question == '' || answer == ''){
+            feedback.classList.remove("hidden")
+            setTimeout(() =>{
+                feedback.classList.add("hidden")
+            }, 3000)
+        }else{
+            let card = new Card(id, question, answer)
+            id++
+            data.push(card)
+            op.addCard(card)
+            op.addLocalStorage(data)
+        }
     })
 
-    // CHECANDO AS INFORMAÇÕES
-    flashcard.prototype.checkinfo = function(){
-        if(this.answer == "" || this.question == ""){
-            feedback.classList.remove("hidden")
-            setTimeout(() => {
-                feedback.classList.add("hidden")
-            }, 2000)
-        }else{
-            let display = new Display()
-            flashcards.push(this)
-            display.passinfo(flashcards)
-            let item = [flashcards[flashcards.length-1]]
-            getlocalstorage(item)
-            form.classList.add("hidden")
+    //Manipulando o flash-card DELETE/EDIT/SHOW-HIDE
+
+    // Construtor da função responsável pela exibição
+    function OP(){
+        OP.prototype.recolherLocalStorage = function(){
+            let savedlocalstorage = localStorage.getItem("cards")
+            if(savedlocalstorage){
+                savedlocalstorage = JSON.parse(savedlocalstorage)
+                return savedlocalstorage
+            }else{
+                return savedlocalstorage = []
+            }
+        }
+
+        OP.prototype.addCard = function(item){
+            dvcards.insertAdjacentHTML("beforeend", `<div class="card"><h2>${item.question}</h2><span>Show/Hide Answer</span><p class="hidden">${item.answer}</p><div class="bts"><button class="edit" data-id="${item.id}">EDIT</button><button class="delete" data-id="${item.id}">DELETE</button></div></div>`)
+        }
+
+        OP.prototype.showCardForm = function(){
+            cardform.classList.remove("hidden")
+        }
+
+        OP.prototype.hideCardForm = function(){
+            cardform.classList.add("hidden")
+        }
+
+        OP.prototype.addLocalStorage = function(data){
+            localStorage.clear()
+            localStorage.setItem("cards", JSON.stringify(data))
         }
     }
 
-
-    // PASSANDO AS INFORMAÇÕES PARA O LOCALSTORAGE
-    Display.prototype.passinfo = function(cards){
-            localStorage.setItem("cards", JSON.stringify(cards))
+    // Objeto card
+    function Card(id, question, answer){
+        this.id = id
+        this.question = question
+        this.answer = answer
     }
-
-    // INSERINDO O HTML
-    function getlocalstorage(cards){
-        let dvcards = document.getElementById("cards")
-        cards.forEach((card) =>{
-            let cor = colors[Math.round(Math.random()*colors.length-1)]
-            dvcards.insertAdjacentHTML("beforeend", `<div class="card" style="background-color: ${cor};"><h2>${card.question}</h2><span onclick="muda(this)">Show/Hide Answer</span><p class="hidden">${card.answer}</p><div class="bts"><button class="edit">EDIT</button><button class="delete">DELETE</button></div></div>`)
-
-            // BOTÃO DELETE
-            let btdel = document.getElementsByClassName("delete")
-            btdel[btdel.length-1].addEventListener("click", (e) =>{
-                let name = e.currentTarget.parentElement.parentElement.querySelector("h2").innerText
-                e.currentTarget.parentElement.parentElement.remove()
-                let data = flashcards.filter((ob) =>{
-                    return ob.question != name
-                })
-                flashcards = data
-                localStorage.setItem("cards", JSON.stringify(flashcards))
-            })
-            
-            // BOTÃO EDIT
-            let btedit = document.getElementsByClassName("edit")
-            btedit[btedit.length-1].addEventListener("click", (e) =>{
-                let pergunta = e.currentTarget.parentElement.parentElement.querySelector("h2").innerText
-                let resposta = e.currentTarget.parentElement.parentElement.querySelector("p").innerText
-
-                e.currentTarget.parentElement.parentElement.remove()
-                flashcards = flashcards.filter((ob) =>{
-                    return ob.question != pergunta
-                })
-                localStorage.setItem("cards", JSON.stringify(flashcards))
-
-                form.classList.remove("hidden")
-                let que = document.getElementById("questao").innerText = pergunta
-                let re = document.getElementById("resposta").innerText = resposta
-            })
-        })
-    }
-})()
-//SHOW/HIDE
-function muda(e){
-    console.log(e.nextElementSibling.classList.toggle("hidden"))
 }
+
+window.addEventListener("load", inicia)
